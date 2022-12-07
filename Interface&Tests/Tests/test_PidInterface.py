9506d82a72f3a5cbbab1a6a6254edff4c4c673b8
LamaShawahny <74476764+LamaShawahny@users.noreply.github.com>
2022-12-08 01:51:15 +0200

    Delete Interface&Tests directory

--- Interface&Tests/Tests/test_PidInterface.py
+++
@@ -1,7 +0,0 @@
-from unittest import TestCase
-from Algorithm.PidInterface import PidInterface
-
-class TestPidInterface(TestCase):
-    def test_pid(self):
-        prd = PidInterface()
-        self.assertIsNone(prd.pid(0.004, 0.2332))
